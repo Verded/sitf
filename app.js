ceef9d254a03258f15dc8c8c9ad92e8f970279b8
Yuinshiel <wee.jiawei.kevan@dhs.sg>
2016-07-22 17:14:39 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,12 @@
+
+$(function() {
+    $("#getjokes").click(
+        function() {
+            $.get("/jokes",function(data){
+     
+                $("#setup").html(data.setup);
+                 $("#punchline").html(data.punchline);
+            },"json")
+        }
+    );
+});
